43441945a7516ccbb47e6c8b74018c74d730e6fa
Nabil Akhunjee <nbakh16@gmail.com>
2026-03-01 00:11:18 +0600

    updated contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nbakh16.github.io',
-  base: '/my-portfolio',
+  // site: '#',
+  // base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
