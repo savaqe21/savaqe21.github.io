2d808c3aeb3b1f3f83b991274fc75b25edf28cfe
Nabil Akhunjee <nbakh16@gmail.com>
2026-02-28 13:34:19 +0600

    updated image path and site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nbakh16.github.io',
-  base: '/my-portfolio',
+  site: 'https://akhunjee.netlify.app',
   vite: {
     plugins: [tailwindcss()]
   },
